310ec4f85f49d71c9c900b99f2151d0683787433
mzq <mzq16@tsinghua.org.cn>
2023-12-07 11:11:48 +0800

    to new version

--- gym_ENV/__init__.py
+++ gym_ENV/__init__.py
@@ -1,18 +1,17 @@
-# from gymnasium.envs.registration import register
-from gym.envs.registration import register
+from gymnasium.envs.registration import register
+# from gym.envs.registration import register
 from . import envs
-'''
+
 register(
-     id="gym_ENV/Terminal_Env-v0",
+     id="Terminal_Env-v0",
      entry_point="gym_ENV.envs:Terminal_Env",
      max_episode_steps=300,
 )
 
-
-the following is old version
 '''
+the following is old version
 register(
     id='Terminal_Env-v0',
     entry_point='gym_ENV.envs:Terminal_Env',
 ) 
-
+'''
